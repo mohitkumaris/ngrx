ade402baad26ebbcbf3ac4e798b9b1ae429022cf
mohitkumaris <mohitkumaris@gmail.com>
2019-07-27 15:09:27 +0530

    intial commit

---
+++ basic/src/actions/tutorial.action.ts
@@ -0,0 +1,14 @@
+// Section 1
+import { Injectable } from '@angular/core'
+import { Action } from '@ngrx/store'
+import { Tutorial } from './../models/models'
+
+// Section 2
+export const ADD_TUTORIAL       = '[TUTORIAL] Add'
+export const REMOVE_TUTORIAL    = '[TUTORIAL] Remove'
+
+
+export class AddTutorial implements Action {
+
+  
+}
